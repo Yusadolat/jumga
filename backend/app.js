54809dc0ab687a2f93065c703b002903a94e9840
Yusadolat <yusadolat@gmail.com>
2020-12-31 07:59:28 +0100

    Server Bootstrap :zap:

---
+++ backend/app.js
@@ -0,0 +1,23 @@
+import express from "express";
+import dotenv from "dotenv";
+
+import connectDB from "./config/db.js";
+
+dotenv.config();
+
+connectDB();
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Wowza, API is running....");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(
+  PORT,
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+);
