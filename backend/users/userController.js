26989bbeb624eb74d88b2b2a255e98ea70e0fa4b
Yusadolat <yusadolat@gmail.com>
2021-01-05 09:58:25 +0100

    get User Details :zap:

--- backend/users/userController.js
+++ backend/users/userController.js
@@ -134,4 +134,20 @@ console.log(subaccount_id)
 }
   
 });
-export { loginUser, registerUser };
+
+const getUserById = asyncHandler(async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id)
+  
+    res.status(200).json({user})
+    
+  } catch (error) {
+    res.status(500).send({status: "Failed", message: error.message})
+    
+  }
+  
+
+})
+
+
+export { loginUser, registerUser, getUserById };
--- backend/users/userRoutes.js
+++ backend/users/userRoutes.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import {registerUser, loginUser} from './userController.js'
+import {registerUser, loginUser, getUserById} from './userController.js'
 const router = express.Router()
 
 
 router.route('/register').post(registerUser)
 router.route('/login').post(loginUser)
+router.route('/:id').get(getUserById)
 
 
 
