c1cc61d41872b524e2ae31a715a2cc747a410722
Yusadolat <yusadolat@gmail.com>
2021-01-07 20:48:23 +0100

    Feat: User Status :zap:

--- backend/users/userController.js
+++ backend/users/userController.js
@@ -1,21 +1,21 @@
 import asyncHandler from "express-async-handler";
-import axios from 'axios'
-import generateToken from '../utils/generateToken.js'
+import axios from "axios";
+import generateToken from "../utils/generateToken.js";
 import User from "../users/userModel.js";
 
 const loginUser = asyncHandler(async (req, res) => {
-    const { email, password } = req.body
-  
-    const user = await User.findOne({ email })
-  
-    if (user && (await user.matchPassword(password))) {
-      res.json({ status: "Sucess", user, token: generateToken(user._id) 
-      })
-    } else {
-      res.status(401).send({status: "Failed", message: "Incorrect Login Details" });
-      
-    }
-  })
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (user && (await user.matchPassword(password))) {
+    res.json({ status: "Sucess", user, token: generateToken(user._id) });
+  } else {
+    res
+      .status(401)
+      .send({ status: "Failed", message: "Incorrect Login Details" });
+  }
+});
 
 const registerUser = asyncHandler(async (req, res) => {
   const {
@@ -28,16 +28,15 @@ const registerUser = asyncHandler(async (req, res) => {
     bank_name,
     bank_code,
     account_number,
-    isMerchant
+    isMerchant,
   } = req.body;
-  
 
   const userExists = await User.findOne({ email });
   if (userExists) {
-    res.status(400).send({status: "Failed", message: "User already exists"});
+    res.status(400).send({ status: "Failed", message: "User already exists" });
   }
 
-  if (bank_name === ""){
+  if (bank_name === "") {
     const freshUser = await User.create({
       fullname,
       email,
@@ -49,7 +48,7 @@ const registerUser = asyncHandler(async (req, res) => {
       bank_code,
       account_number,
     });
-  
+
     if (freshUser) {
       res.status(201).json({
         _id: freshUser._id,
@@ -57,85 +56,98 @@ const registerUser = asyncHandler(async (req, res) => {
         email: freshUser.email,
         isMerchant: freshUser.isMerchant,
         token: generateToken(freshUser._id),
-      })
-    
+      });
     } else {
-      res.status(400).send({status: "Failed", message: error})
-      
+      res.status(400).send({ status: "Failed", message: error });
     }
-  }else{
-  //Create Subaccount
+  } else {
+    //Create Subaccount
 
-  var data =  JSON.stringify({
-    "account_bank": bank_code,
-    "account_number": account_number,
-    "business_name": business_name,
-    "business_email": email,
-    "business_mobile": phone_number,
-    "country": country,
-    "split_type": "percentage",
-    "split_value": 0.093
-  })
-  var config = {
-    method: 'post',
-    url: 'https://api.flutterwave.com/v3/subaccounts',
-    headers: { 
-      'Authorization': `Bearer ${process.env.LIVE_API_KEY}`, 
-      'Content-Type': 'application/json'
-    },
-    data : data
-  };
-  
-  axios(config)
-  .then(async function (response)  {
-      //res.status(200).json(response.data.data);
- const {subaccount_id} = response.data.data
-console.log(subaccount_id)
- const newUser = await User.create({
-    fullname,
-    email,
-    password,
-    phone_number,
-    country,
-    business_name,
-    bank_name,
-    bank_code,
-    account_number,
-    subaccount_id,
-    isMerchant
-  });
-console.log(newUser)
-  if (newUser) {
-    res.status(201).json({  
-      token: generateToken(newUser._id),
-      data: newUser
-    }).catch(function (error) {
-      res.status(400).json({status: "Failed", message: error.message});
+    var data = JSON.stringify({
+      account_bank: bank_code,
+      account_number: account_number,
+      business_name: business_name,
+      business_email: email,
+      business_mobile: phone_number,
+      country: country,
+      split_type: "percentage",
+      split_value: 0.093,
     });
-    
+    var config = {
+      method: "post",
+      url: "https://api.flutterwave.com/v3/subaccounts",
+      headers: {
+        Authorization: `Bearer ${process.env.LIVE_API_KEY}`,
+        "Content-Type": "application/json",
+      },
+      data: data,
+    };
+
+    axios(config)
+      .then(async function (response) {
+        //res.status(200).json(response.data.data);
+        const { subaccount_id } = response.data.data;
+        console.log(subaccount_id);
+        const newUser = await User.create({
+          fullname,
+          email,
+          password,
+          phone_number,
+          country,
+          business_name,
+          bank_name,
+          bank_code,
+          account_number,
+          subaccount_id,
+          isMerchant,
+        });
+        console.log(newUser);
+        if (newUser) {
+          res
+            .status(201)
+            .json({
+              token: generateToken(newUser._id),
+              data: newUser,
+            })
+            .catch(function (error) {
+              res
+                .status(400)
+                .json({ status: "Failed", message: error.message });
+            });
+        }
+      })
+      .catch(function (error) {
+        //console.log(error.response.data.message)
+        res.status(400).send({ status: "Failed", message: error.message });
+      });
   }
-  })
-  .catch(function (error) {
-    //console.log(error.response.data.message)
-    res.status(400).send({status: "Failed", message: error.message});
-  });
-}
-  
 });
 
 const getUserById = asyncHandler(async (req, res) => {
   try {
-    const user = await User.findById()
-  
-    res.status(200).json({user})
-    
+    const user = await User.findById();
+
+    res.status(200).json({ user });
   } catch (error) {
-    res.status(500).send({status: "Failed", message: error.message})
-    
+    res.status(500).send({ status: "Failed", message: error.message });
   }
-  
+});
 
-})
+const updateUser = asyncHandler(async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    if (user) {
+      user.account_status = req.body.account_status || user.account_status;
+      const updatedUser = await user.save();
 
+      res.json({
+        token: generateToken(updatedUser._id),
+        updatedUser,
+      });
+    }
+  } catch (err) {
+    res.status(404).send({ status: "Failed", message: error.message });
+  }
+});
 
-export { loginUser, registerUser, getUserById };
+export { loginUser, registerUser, getUserById, updateUser };
--- backend/users/userRoutes.js
+++ backend/users/userRoutes.js
@@ -1,12 +1,14 @@
 import express from 'express';
-import {registerUser, loginUser, getUserById} from './userController.js'
+import {registerUser, loginUser, getUserById, updateUser} from './userController.js'
+import { protect } from '../middleware/authMiddleware.js'
+
 const router = express.Router()
 
 
 router.route('/register').post(registerUser)
 router.route('/login').post(loginUser)
-router.route('/:id').get(getUserById)
+router.route('/:id').get(getUserById).put(protect, updateUser)
 
 
 
-export default router
+export default router;
