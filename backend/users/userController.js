221c299c537f04e3022aeebc46cdd3a3b6ca3784
Yusadolat <yusadolat@gmail.com>
2021-01-15 07:10:42 +0100

    Get User by ID :zap:

--- backend/users/userController.js
+++ backend/users/userController.js
@@ -127,7 +127,7 @@ const getUserById = asyncHandler(async (req, res) => {
   try {
     const user = await User.findById(req.params.id);
 
-    res.status(200).json({ user });
+    res.status(200).json( user );
   } catch (error) {
     res.status(500).send({ status: "Failed", message: error.message });
   }
