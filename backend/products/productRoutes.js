8c238f12137f9c4c9c3ae753186a87b6b91fb64a
Yusadolat <yusadolat@gmail.com>
2021-01-15 08:40:01 +0100

    Create Producbt :zap:

--- backend/products/productRoutes.js
+++ backend/products/productRoutes.js
@@ -3,9 +3,9 @@ import { protect, merchant } from '../middleware/authMiddleware.js'
 
 
 const router = express.Router();
-import { getProducts, getProductById, deleteProduct } from "./productController.js";
+import {createProduct, getProducts, getProductById, deleteProduct } from "./productController.js";
 
-router.route("/").get(getProducts);
+router.route("/").get(getProducts).post(createProduct)
 router
   .route('/:id')
   .get(getProductById)
