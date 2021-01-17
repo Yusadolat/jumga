3a9ab9c566a6734f0a9eb7fb5cd056606a28f9d5
Yusadolat <yusadolat@gmail.com>
2021-01-17 14:17:26 +0100

    remove custom cors :zap:

--- backend/app.js
+++ backend/app.js
@@ -19,14 +19,14 @@ const app = express();
 
 app.use(express.json());
 app.use(cors())
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept',
-  );
-  next();
-});
+// app.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', '*');
+//   res.header(
+//     'Access-Control-Allow-Headers',
+//     'Origin, X-Requested-With, Content-Type, Accept',
+//   );
+//   next();
+// });
 app.get("/", (req, res) => {
   res.send("Wowza, API is running....");
 });
