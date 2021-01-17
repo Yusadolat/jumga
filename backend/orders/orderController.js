bde993a52182d8266625715ec197e6bfe62b7b36
Yusadolat <yusadolat@gmail.com>
2021-01-17 17:09:24 +0100

    New Order Schema :zap:

--- backend/orders/orderController.js
+++ backend/orders/orderController.js
@@ -5,14 +5,15 @@ const createOrder = asyncHandler(async (req, res) => {
   try {
 
     const order = await Order.create({
+        title: req.body.title,
         user_id: req.body.user,
         product_id: req.body.product_id,
-        orderItems: req.body.orderItems,
-        shippingAddress: req.body.shippingAddress,
-        paymentResult: req.body.paymentResult,
-        paidAt: req.body.paidAt,
-        isDelivered: req.body.isDelivered,
-        deliveredAt: req.body.deliveredAt
+        customer: req.body.customer,
+        amount: req.body.amount,
+        currency: req.body.currency,
+        transaction_id: req.body.transaction_id,
+        tx_ref: req.body.tx_ref,
+        deliveredAt: req.body.flw_ref
     });
 
     res.status(201).json({
